2b1cc1d7621ade5c72bc49b7cd7d2e1709816489
SaulQuadros <126301913+SaulQuadros@users.noreply.github.com>
2025-11-10 10:28:04 -0300

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title='SPAQ App', page_icon='💧', layout='wide')
-st.title('SPAQ App — Dimensionamento de Aquecedores a Gás')
-st.write('Use o menu Pages para navegar.')
